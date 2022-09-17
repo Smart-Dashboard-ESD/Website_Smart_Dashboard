0b216aa92efb55d06b21190780e46105c1781e71
fatihabdi <dotaawtf23@gmail.com>
2022-09-17 13:57:58 +0700

    feat/create dashboard user page

--- src/pages/DashboardUser.js
+++ src/pages/DashboardUser.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function DashboardUser() {
   return (
-    <div className="">
-      <div className=""></div>
-      <h1>Homepage</h1>
+    <div className="ml-[15px]">
+      <DoughnutChartSection />
     </div>
   );
 }
