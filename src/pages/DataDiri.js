5eb2c101b4ecf5c2b3475b2348d593ef4514330f
fatihabdi <dotaawtf23@gmail.com>
2022-09-28 06:08:16 +0700

    feat/update pagination on top

--- src/pages/DataDiri.js
+++ src/pages/DataDiri.js
@@ -1,6 +1,5 @@
 import React from "react";
 import FormDataDiri from "../components/FormDataDiri";
-import PaginationDataDiri from "../components/PaginationDataDiri";
 import SidebarAlt from "../components/SidebarAlt";
 
 export default function DataDiri() {
@@ -8,7 +7,25 @@ export default function DataDiri() {
     <div className="w-screen h-screen flex bg-[#F3F3F3]">
       <SidebarAlt />
       <div>
-        <PaginationDataDiri />
+        <div className="absolute top-0 right-[202px] flex items-center mt-8">
+          <div className="text-white rounded-full flex items-center justify-center bg-Primary-Normal w-[35px] h-[35px]">
+            1
+          </div>
+          <div className="">
+            <h1 className="ml-2 text-base font-semibold text-Black-Normal">
+              Isi data diri
+            </h1>
+          </div>
+          <div className="w-[40px] border-b-2 border-Greyscale-Normal border-dotted border-slate-400 m-4"></div>
+          <div className="text-white rounded-full flex items-center justify-center bg-Greyscale-Normal w-[35px] h-[35px] ">
+            2
+          </div>
+          <div className="">
+            <h1 className="ml-2 text-base font-semibold text-Greyscale-Normal">
+              Register
+            </h1>
+          </div>
+        </div>
         <FormDataDiri />
       </div>
     </div>
