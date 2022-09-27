e4fae7d261aa5d41607f8eba02579373226c48cd
fatihabdi <dotaawtf23@gmail.com>
2022-09-28 06:11:57 +0700

    feat/remove kode promo button

--- src/components/PaymentDetail.js
+++ src/components/PaymentDetail.js
@@ -1,6 +1,4 @@
 import React from "react";
-import diskon from "../assets/img/diskon.png";
-import { HiChevronRight } from "react-icons/hi";
 
 export default function PaymentDetail() {
   return (
@@ -8,7 +6,7 @@ export default function PaymentDetail() {
       <h1 className="font-semibold text-lf text-Black-Normal">
         Payment Detail
       </h1>
-      <div className="w-[450px] h-[482px] bg-white mt-4 rounded">
+      <div className="w-[450px] h-[422px] bg-white mt-4 rounded">
         <div className="flex justify-between pt-5 ml-5 mr-[26px]">
           <span>
             <h1 className="text-[25px] font-medium text-Black-Normal">
@@ -50,16 +48,6 @@ export default function PaymentDetail() {
               : Rp. 168.000,-
             </span>
           </h1>
-        </div>
-        <div
-          className="w-[410px] h-[48px] mx-5 bg-white border border-Greyscale-NormalActive rounded mt-[30px] flex items-center"
-          onClick={() => console.log("Button di klik")}
-        >
-          <img src={diskon} alt="diskon" className="ml-[16px]" />
-          <h1 className="ml-2 text-sm font-medium text-Greyscale-DarkActive">
-            Pakai Kode Promo
-          </h1>
-          <HiChevronRight className="ml-auto mr-[16px] text-Primary-Normal w-4 h-4" />
         </div>
         <div className="w-[410px] h-[39px] bg-Primary-Normal mx-5 rounded mt-[50px] flex items-center justify-center text-white text-base font-medium">
           Konfirmasi Pembayaran
