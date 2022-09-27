c650499010b076f5e01f7435db9cf82605915600
fatihabdi <dotaawtf23@gmail.com>
2022-09-27 09:08:50 +0700

    feat/update modal pop up centered when scale up

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -90,6 +90,8 @@ export default function SignUpForm() {
           onClose={onClose}
           closeOnOverlayClick={false}
           size={"lg"}
+          isCentered
+          motionPreset="scale"
         >
           <ModalOverlay />
           <ModalContent>
