797e18eba668c920c0e61c7c2842d6e8747c5517
fatihabdi <dotaawtf23@gmail.com>
2022-10-01 20:05:31 +0700

    feat/add count up number on middle of chart

--- src/components/Chart/DoughnutChartMinggu.js
+++ src/components/Chart/DoughnutChartMinggu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart, ArcElement, Tooltip } from "chart.js";
+import CountUp from "react-countup";
 Chart.register(ArcElement, Tooltip);
 
 export default function DoughnutChartMinggu() {
@@ -29,6 +30,12 @@ export default function DoughnutChartMinggu() {
           responsive: true,
         }}
       />
+      <div className="absolute z-10 top-[60px] right-[65px] text-[31px] font-semibold text-Primary-Normal">
+        <CountUp end={280} duration={1} />
+        <h1 className="text-[20px] font-medium text-Greyscale-Normal ml-4 -mt-3">
+          m3
+        </h1>
+      </div>
     </div>
   );
 }
