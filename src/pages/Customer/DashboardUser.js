19d4f5f25a889631aa831ccd9438f93c20c34203
fatihabdi <dotaawtf23@gmail.com>
2022-10-23 18:45:56 +0700

    feat/fix bugs

--- src/pages/Customer/DashboardUser.js
+++ src/pages/Customer/DashboardUser.js
@@ -3,16 +3,20 @@ import BillingDate from "../../components/BillingDate";
 import DoughnutChartSection from "../../components/DoughnutChartSection";
 import History from "../../components/History";
 import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export default function DashboardUser() {
   return (
-    <div className="h-screen default:w-[1150px]">
-      <Header />
-      <div className="flex ml-[15px]">
-        <DoughnutChartSection />
-        <BillingDate />
+    <div className="flex w-screen default:min-h-screen 2xl:h-[70px] 4xl:h-[1080px] App bg-[#F3F3F3]">
+      <Sidebar />
+      <div className="h-screen default:w-[1150px]">
+        <Header />
+        <div className="flex ml-[15px]">
+          <DoughnutChartSection />
+          <BillingDate />
+        </div>
+        <History />
       </div>
-      <History />
     </div>
   );
 }
