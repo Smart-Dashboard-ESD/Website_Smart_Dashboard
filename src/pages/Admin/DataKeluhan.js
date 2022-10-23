64c5314cb2d01476ffe9e35dfb8b3b4154f2d74e
fatihabdi <dotaawtf23@gmail.com>
2022-10-23 09:53:15 +0700

    feat/add 1366x768 screen

--- src/pages/Admin/DataKeluhan.js
+++ src/pages/Admin/DataKeluhan.js
@@ -7,8 +7,8 @@ export default function DataKeluhan() {
     <div className="w-screen h-screen flex bg-[#F3F3F3] overflow-y-auto">
       <SidebarAdmin />
       <div>
-        <div className="default:mt-[30px] default:ml-[20px]">
-          <h1 className="font-semibold default:text-[20px] text-Black-Normal">
+        <div className="mt-[30px] ml-[20px]">
+          <h1 className="font-semibold text-[20px] text-Black-Normal">
             Data Keluhan
           </h1>
           <p className="font-medium text-Greyscale-NormalHover">
