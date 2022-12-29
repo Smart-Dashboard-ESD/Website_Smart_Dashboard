fde9a53516e6c810d2643e2c6cbfae4ee989db24
fatihabdi <dotaawtf23@gmail.com>
2022-12-29 18:15:09 +0700

    feat/responsive bg at billing dashboard

--- src/pages/Billing/HargaAir.js
+++ src/pages/Billing/HargaAir.js
@@ -3,7 +3,7 @@ import SidebarBilling from "../../components/Sidebar/SidebarBilling";
 
 export default function HargaAir() {
   return (
-    <div className="w-screen h-screen flex bg-[#F3F3F3]">
+    <div className="w-screen default:min-h-screen 2xl:h-[768px] 4xl:h-[1080px] flex bg-[#F3F3F3]">
       <SidebarBilling />
       <div className=""></div>
     </div>
